c4b92d90794100c7c777cf14841a7c23e560d800
Erick20247 <Torot0086@gmail.vom>
2024-05-25 14:56:12 -0400

    eh creado el contexto

---
+++ registrotecnico/registrotecnico/DAL/Contexto.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using registrotecnico.Models;
+
+namespace registrotecnico.DAL
+{
+    public class Contexto : DbContext
+    {
+        
+       
+            
+
+            public Contexto(DbContextOptions<Contexto> options)
+            : base(options) { }
+        public DbSet<Tecnicos> tecnicos { get; set; }
+    }
+}
